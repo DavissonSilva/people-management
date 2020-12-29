318d794ad653a7129f626a7653333d860a850907
dcardosi <davisson.cardoso.silva@everis.com>
2020-12-29 12:57:39 -0300

    Implementação da class empresa

--- src/main/java/com/logicalis/entity/Empresa.java
+++ src/main/java/com/logicalis/entity/Empresa.java
@@ -1,5 +1,66 @@
 package com.logicalis.entity;
 
-public class Empresa {
+import java.io.Serializable;
+import java.util.Date;
+import java.util.List;
 
+import javax.annotation.Generated;
+import javax.persistence.CascadeType;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.PrePersist;
+import javax.persistence.PreUpdate;
+
+import com.sun.istack.NotNull;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+public class Empresa implements Serializable{
+
+	private static final long serialVersionUID = 876545678987655678L;
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	
+	private Long id;
+	@NotNull
+	private String razaoSocial;
+	@NotNull
+	private String cnpj;
+	@NotNull
+	private Date dataCriacao;
+	@NotNull
+	private Date dataAtualizacao;
+	@OneToMany(mappedBy = "empresa", fetch = FetchType.LAZY, cascade = CascadeType.ALL)
+	private List<Funcionario> funcionario;
+	
+	public Empresa() {
+	}
+
+	@Override
+	public String toString() {
+		return "Empresa [id=" + id + ", razaoSocial=" + razaoSocial + ", cnpj=" + cnpj + ", dataCriacao=" + dataCriacao
+				+ ", dataAtualizacao=" + dataAtualizacao + "]";
+	}
+	
+	@PreUpdate
+	public void preUpdate(){
+		this.dataAtualizacao = new Date();
+	}
+	
+	@PrePersist
+	public void prePersist() {
+		final Date atual = new Date();
+		this.dataAtualizacao = atual;
+		this.dataCriacao = atual;
+	}
+	
+	
 }
