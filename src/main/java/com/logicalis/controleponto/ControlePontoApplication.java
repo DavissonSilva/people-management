58beebf28e40da40f0a9af1c78fa7f889b97fecb
dcardosi <davisson.cardoso.silva@everis.com>
2020-12-29 10:10:39 -0300

    vercionando codigo fonte

---
+++ src/main/java/com/logicalis/controleponto/ControlePontoApplication.java
@@ -0,0 +1,13 @@
+package com.logicalis.controleponto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ControlePontoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ControlePontoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/logicalis/controleponto/ControlePontoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.logicalis.controleponto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ControlePontoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
