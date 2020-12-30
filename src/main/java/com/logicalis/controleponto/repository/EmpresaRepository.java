65ae8f96db7196bb3c298a5381ad78acc1507041
dcardosi <davisson.cardoso.silva@everis.com>
2020-12-30 16:21:17 -0300

    implementação da class PasswordUtils

--- src/main/java/com/logicalis/controleponto/repository/EmpresaRepository.java
+++ src/main/java/com/logicalis/controleponto/repository/EmpresaRepository.java
@@ -5,6 +5,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import com.logicalis.controleponto.entity.Empresa;
 
 public interface EmpresaRepository extends JpaRepository<Empresa, Long> {
-	
+
 	Empresa findByCnpj(String cnpj);
 }
---
+++ src/main/java/com/logicalis/controleponto/utils/PasswordUtils.java
@@ -0,0 +1,28 @@
+package com.logicalis.controleponto.utils;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+public class PasswordUtils {
+
+	private static final Logger log = LoggerFactory.getLogger(PasswordUtils.class);
+	
+	public PasswordUtils() {
+	}
+	
+	/**
+	 * Gera um hosh utilizando o BCrypt
+	 * 
+	 */
+	 
+	 public static String gerarBCrypt(String senha) {
+		if(senha == null) {
+			return senha;
+		} 
+		log.info("Gerando hash com o BCrypt");
+		BCryptPasswordEncoder bCryptEncoder = new BCryptPasswordEncoder();
+		return bCryptEncoder.encode(senha);
+		
+	 }
+}
