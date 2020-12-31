dc4cb0ec95989b1c207571593fc8f78ddc3550a2
dcardosi <davisson.cardoso.silva@everis.com>
2020-12-31 09:04:28 -0300

    Criação e implementação da class LancamentoRepository

---
+++ src/main/java/com/logicalis/controleponto/repository/LancamentoRepository.java
@@ -0,0 +1,22 @@
+package com.logicalis.controleponto.repository;
+
+import java.util.List;
+
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
+
+import com.logicalis.controleponto.entity.Lancamentos;
+
+@NamedQueries({
+	@NamedQuery(name = "LancamentoRepository.findByFuncionariosId",
+			query = "SELECT lanc FROM Lancamento lanc WHERE lanc.funcionarios.id = :funcionarioId")})
+public interface LancamentoRepository extends JpaRepository<Lancamentos, Long> {
+	
+	List<Lancamentos> findByFuncionariosId(@Param("funcionarioId") Long funcionarioId);
+	Page<Lancamentos> findByFuncionariosId(@Param("funcionarioId") Long funcionarioId, Pageable pegeable);
+}
