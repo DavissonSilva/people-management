2ebb48a0b05b90e6ae2f65453443c428adaa8c52
dcardosi <davisson.cardoso.silva@everis.com>
2021-03-13 13:32:46 -0300

    Continuação da criação do recurso

--- src/main/java/com/logicalis/controleponto/resource/LancamentosResource.java
+++ src/main/java/com/logicalis/controleponto/resource/LancamentosResource.java
@@ -3,24 +3,27 @@ package com.logicalis.controleponto.resource;
 import java.text.SimpleDateFormat;
 import java.util.Optional;
 
+import org.apache.commons.lang3.EnumUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
-import org.springframework.data.domain.Sort.Direction;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.ObjectError;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.logicalis.controleponto.dto.LancamendoDto;
+import com.logicalis.controleponto.entity.Funcionario;
 import com.logicalis.controleponto.entity.Lancamentos;
+import com.logicalis.controleponto.enums.TipoEnum;
 import com.logicalis.controleponto.implementandoService.EmpresaServiceImpl;
 import com.logicalis.controleponto.response.Response;
 import com.logicalis.controleponto.service.FuncionarioService;
@@ -41,9 +44,6 @@ public class LancamentosResource {
 	@Autowired
 	private FuncionarioService funcionarioService;
 	
-	@Value("${paginacao.qtd_por_pagina}")
-	private int qtdPorpagina;
-	
 	public LancamentosResource() {
 	}
 	
@@ -62,6 +62,83 @@ public class LancamentosResource {
 		return ResponseEntity.ok(response);
 	}
 	
+	@GetMapping(value = "/{id}")
+	public ResponseEntity<Response<LancamendoDto>> listaPorId(@PathVariable("id")Long id){
+		LOG.info("Buscando lancamento por ID", id);
+		
+		Response<LancamendoDto> response = new Response<LancamendoDto>();
+		Optional<Lancamentos> lancamento = this.lancamentoService.buscarporId(id);
+		
+		if(!lancamento.isPresent()){
+			LOG.info("Lancamento não encontrado para o id {}  ", id);
+			response.getErrors().add("Lancamendo não encontrado para o id" + id);
+			return ResponseEntity.badRequest().body(response);				
+		}
+		
+		response.setData(this.converterLancamentoDto(lancamento.get()));
+		
+		return ResponseEntity.ok(response);
+	}
+	@PostMapping
+	public ResponseEntity<Response<LancamendoDto>> salvandolancamento(@RequestBody LancamendoDto dto,
+			BindingResult bindingResult) throws Exception{
+		LOG.info("Adicionando lançamento:{}", dto.toString());
+		Response<LancamendoDto> response = new Response<LancamendoDto>();
+		
+		this.validaFuncionario(dto, bindingResult);
+		
+		Lancamentos lancamentos = this.converterDtoparaLancamento(dto, bindingResult);
+		
+		if(bindingResult.hasErrors()) {
+			LOG.info("Erro validando lançamento: {}", bindingResult.getAllErrors());
+			bindingResult.getAllErrors().forEach( error -> response.getErrors().add(error.getDefaultMessage()));
+			return ResponseEntity.badRequest().body(response);
+		}
+		lancamentos = this.lancamentoService.persistir(lancamentos);
+		response.setData(this.converterLancamentoDto(lancamentos));
+		return ResponseEntity.ok(response);
+		
+	}
+	
+	private void validaFuncionario(LancamendoDto dto, BindingResult bindingResult){
+		if(dto.getFuncionarioId() == null) {
+			bindingResult.addError(new ObjectError("Funcionario", "Funcionario não informado."));
+			return;
+		}
+		LOG.info("Validando funcionario por id {}:", dto.getFuncionarioId());
+		Optional<Funcionario> funcionario = this.funcionarioService.buscaPorId(dto.getFuncionarioId());
+		if(!funcionario.isPresent()){
+			bindingResult.addError(new ObjectError("Funcionario", "Funcionario não encontrado. ID inexistente."));
+		}
+	}
+
+	private Lancamentos converterDtoparaLancamento(LancamendoDto dto, BindingResult bindingResult) throws Exception {		
+		Lancamentos lancamentos = new Lancamentos();
+		if(dto.getId().isPresent()){
+			Optional<Lancamentos> lanc = this.lancamentoService.buscarporId(dto.getId().get());
+			if(lanc.isPresent()) {
+				lancamentos = lanc.get();
+			}else {
+				bindingResult.addError(new ObjectError("Lancamento", "Lancamento não encontrado"));
+			}
+		}else{
+			lancamentos.setFuncionarios(new Funcionario());
+			lancamentos.getFuncionarios().setId(dto.getFuncionarioId());
+		}
+		
+		lancamentos.setData(this.DATE_FORMAT.parse(dto.getData()));
+		lancamentos.setDescricao(dto.getDescricao());
+		lancamentos.setLocalizacao(dto.getLocalizacao());
+		
+		if(EnumUtils.isValidEnum(TipoEnum.class, dto.getTipo())){
+		 lancamentos.setTipo(TipoEnum.valueOf(dto.getTipo()));
+		}else {
+			bindingResult.addError(new ObjectError("tipo", "Tipo invalido"));
+		}
+		
+		return lancamentos;
+		
+	}
 	private LancamendoDto converterLancamentoDto(Lancamentos lancamentos) {
 		LancamendoDto dto = new LancamendoDto();
 		
